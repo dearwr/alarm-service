3a723ef2d21a5fb69b8bb4f585553987658099e9
wangrong <1096371950@qq.com>
2020-07-15 10:29:44 +0800

    update

--- src/main/java/com/hchc/alarm/task/AceWillTask.java
+++ src/main/java/com/hchc/alarm/task/AceWillTask.java
@@ -1,13 +1,14 @@
 package com.hchc.alarm.task;
 
+import com.hchc.alarm.pack.Output;
+import com.hchc.alarm.util.DatetimeUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.util.DateUtils;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 /**
  * @author wangrong
@@ -17,21 +18,39 @@ import java.util.List;
 @Slf4j
 public class AceWillTask {
 
-    private static final String AceWill_URL = "http://120.25.75.153:9500/";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String AceWill_DISH_URL = "http://120.25.75.153:9500/sync/basic?hqId={}&type=dishkind,dishunit,dish,menudish";
+    private static final String AceWill_DONE_URL = "http://120.25.75.153:9500/sync/rePushAndDone?hqId={}&branches={}&startDay={}&endDay={}";
 
 
-//    @Scheduled(cron = "0 10 23 * * ?")
-//    public void scientist() {
-//        AceWillPushUtil.sync(1932L);
-//    }
-//
-//    @Scheduled(cron = "0 0 23 * * ?")
-//    public void upload2305() {
-//        long hqId = 1932;
-//        List<Long> branchIds = Arrays.asList(2480L, 2481L, 2482L, 2483L, 2484L, 2485L, 4907L, 5059L);
-//
-//        Date date = new Date();
-//        String today = DateUtils.format(date, "yyyy-MM-dd");
-//        AceWillPushUtil.upload("Scientist", hqId, branchIds, today);
-//    }
+    @Scheduled(cron = "0 50 23 * * ?")
+    public void scientist() {
+        long hqId = 1932L;
+        String branches = "2480,2481,2482,2483,2484,2485,4907,5059";
+        String hqName = "scientist";
+        String startDay = DatetimeUtil.dayText(new Date());
+        String endDay = startDay;
+        doSync(hqId, branches, hqName, startDay, endDay);
+    }
+
+    private void doSync(long hqId, String branches, String hqName, String startDay, String endDay) {
+        log.info("{} start push dish", hqName);
+        Output result = restTemplate.getForObject(AceWill_DISH_URL, Output.class, hqId);
+        if (result != null && "0".equals(result.getCode())) {
+            log.info("{} push dish success", hqName);
+        }else {
+            log.info("{} push dish fail, result :{}", hqName, result);
+            return;
+        }
+        log.info("{} start push order and done", hqName);
+        String response = restTemplate.getForObject(AceWill_DONE_URL, String.class, hqId, branches, startDay, endDay);
+        if (response != null && "ok".equals(response)) {
+            log.info("{} push order and done success", hqName);
+        }else {
+            log.info("{} push order and done fail, result:{}", hqName, response);
+        }
+    }
+
 }
