ef060a53b983d477491cd70e681bea82b35d912a
wangrong <13237007676>
2020-07-25 14:03:13 +0800

    update

--- src/main/java/com/hchc/alarm/task/BranchKdsTask.java
+++ src/main/java/com/hchc/alarm/task/BranchKdsTask.java
@@ -1,37 +1,30 @@
-package com.hchc.alarm.task;
-
-import com.hchc.alarm.dao.hchc.BranchKdsBaseDao;
-import com.hchc.alarm.service.RemoteService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@Slf4j
-public class BranchKdsTask {
-
-    @Autowired
-    private BranchKdsBaseDao branchKdsDao;
-    @Autowired
-    private RemoteService remoteService;
-
-    @Scheduled(cron = "0 */10 * * * ?")
-    public void checkKdsOrders() {
-        log.info("[checkKdsOrders] start check");
-        List<Integer[]> infoList = branchKdsDao.queryCheckInfos();
-        int size;
-        for (Integer[] info : infoList) {
-            size = remoteService.getWxQueueCount(info[1], info[2]);
-            if (size >= 15 && info[3] == 1) {
-                log.info("[checkKdsOrders] close kds fId:{}, hqId:{}, branch:{}", info[0], info[1], info[2]);
-                branchKdsDao.updateOpenState(info[0], 0);
-            } else if (size < 15 && info[3] == 0) {
-                log.info("[checkKdsOrders] open kds fId:{}, hqId:{}, branch:{}", info[0], info[1], info[2]);
-                branchKdsDao.updateOpenState(info[0], 1);
-            }
-        }
-    }
-}
+//package com.hchc.alarm.task;
+//
+//import com.hchc.alarm.dao.hchc.BranchKdsBaseDao;
+//import com.hchc.alarm.service.RemoteService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@Slf4j
+//public class BranchKdsTask {
+//
+//    @Autowired
+//    private BranchKdsBaseDao branchKdsDao;
+//    @Autowired
+//    private RemoteService remoteService;
+//
+//    @Scheduled(cron = "0 46 13 * * ?")
+//    public void checkKdsOrders() {
+//        log.info("[checkKdsOrders] start check");
+//        List<Integer[]> infoList = branchKdsDao.queryCheckInfos();
+//        for (Integer[] info : infoList) {
+//            log.info("[checkKdsOrders] open kds fId:{}, hqId:{}, branch:{}", info[0], info[1], info[2]);
+//            branchKdsDao.updateOpenState(info[0], 1);
+//        }
+//    }
+//}
