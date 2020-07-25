b2edcf3095fd2492adf349a846d28d141fcc96f1
wangrong <13237007676>
2020-07-25 15:07:55 +0800

    update

--- src/main/java/com/hchc/alarm/dao/hchc/KdsOrderDao.java
+++ src/main/java/com/hchc/alarm/dao/hchc/KdsOrderDao.java
@@ -3,6 +3,8 @@ package com.hchc.alarm.dao.hchc;
 import com.hchc.alarm.dao.HcHcBaseDao;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 /**
  * @author wangrong
  * @date 2020-07-23
@@ -11,6 +13,12 @@ import org.springframework.stereotype.Repository;
 public class KdsOrderDao extends HcHcBaseDao {
 
     public int orderComplete(long branchId, String startTime, String endTime) {
+        String sql = "update t_kds_order set f_log_action = 'TAKE_COMPLETE' " +
+                "where f_branchid = ? and f_create_time > ? and f_create_time < ?";
+        return hJdbcTemplate.update(sql, branchId, startTime, endTime);
+    }
+
+    public int orderComplete(long branchId, Date startTime, Date endTime) {
         String sql = "update t_kds_order set f_log_action = 'TAKE_COMPLETE' " +
                 "where f_branchid = ? and f_create_time > ? and f_create_time < ?";
         return hJdbcTemplate.update(sql, branchId, startTime, endTime);
--- src/main/java/com/hchc/alarm/task/BranchKdsTask.java
+++ src/main/java/com/hchc/alarm/task/BranchKdsTask.java
@@ -1,12 +1,14 @@
 //package com.hchc.alarm.task;
 //
 //import com.hchc.alarm.dao.hchc.BranchKdsBaseDao;
-//import com.hchc.alarm.service.RemoteService;
+//import com.hchc.alarm.dao.hchc.KdsOrderDao;
+//import com.hchc.alarm.util.DatetimeUtil;
 //import lombok.extern.slf4j.Slf4j;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.scheduling.annotation.Scheduled;
 //import org.springframework.stereotype.Service;
 //
+//import java.util.Date;
 //import java.util.List;
 //
 //@Service
@@ -16,15 +18,17 @@
 //    @Autowired
 //    private BranchKdsBaseDao branchKdsDao;
 //    @Autowired
-//    private RemoteService remoteService;
+//    private KdsOrderDao kdsOrderDao;
 //
-//    @Scheduled(cron = "0 46 13 * * ?")
+//    @Scheduled(cron = "0 50 14 * * ?")
 //    public void checkKdsOrders() {
 //        log.info("[checkKdsOrders] start check");
 //        List<Integer[]> infoList = branchKdsDao.queryCheckInfos();
+//        Date startTime = DatetimeUtil.dayBegin(new Date());
+//        Date endTime = DatetimeUtil.addMinute(new Date(), -35);
 //        for (Integer[] info : infoList) {
-//            log.info("[checkKdsOrders] open kds fId:{}, hqId:{}, branch:{}", info[0], info[1], info[2]);
-//            branchKdsDao.updateOpenState(info[0], 1);
+//            log.info("[checkKdsOrders] complete order kds fId:{}, hqId:{}, branch:{}", info[0], info[1], info[2]);
+//            kdsOrderDao.orderComplete(info[2], startTime, endTime);
 //        }
 //    }
 //}
