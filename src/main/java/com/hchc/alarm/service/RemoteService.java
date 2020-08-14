49be6e93978ed26a3e88eef5a0d680568b1610b9
wangrong <1096371950@qq.com>
2020-08-14 11:32:59 +0800

    update

--- src/main/java/com/hchc/alarm/service/RemoteService.java
+++ src/main/java/com/hchc/alarm/service/RemoteService.java
@@ -1,7 +1,6 @@
 package com.hchc.alarm.service;
 
 import com.alibaba.fastjson.JSON;
-import com.hchc.alarm.model.MallBranchBO;
 import com.hchc.alarm.model.PushMall;
 import com.hchc.alarm.pack.Output;
 import com.hchc.alarm.pack.QueueInfo;
@@ -9,8 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 /**
  * Created by wangrong 2020/5/13
@@ -21,7 +18,6 @@ import java.util.List;
 public class RemoteService {
 
     public static final String VIP_URL = "https://vip.51hchc.com";
-    public static final String FLIP_TEST_SERVER_URL = "http://47.103.123.87:7676";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -32,13 +28,6 @@ public class RemoteService {
         log.info("[getWxQueueCount] response :{}", JSON.toJSONString(response));
         QueueInfo queueInfo = JSON.parseObject(JSON.toJSONString(response.getData()), QueueInfo.class);
         return queueInfo.getQueueCount();
-    }
-
-    public List<MallBranchBO> queryBranchInfos() {
-        String url = FLIP_TEST_SERVER_URL + "/mallConsole/branchInfos";
-        Output response = restTemplate.getForEntity(url, Output.class).getBody();
-        log.info("[queryBranchInfos] response :{}", JSON.toJSONString(response));
-        return JSON.parseArray(JSON.toJSONString(response.getData()), MallBranchBO.class);
     }
 
     public Output pushOrders(PushMall pushMall, String url) {
